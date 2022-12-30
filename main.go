84b20bec3059372574a8fe213a6042d7ce4cb7f9
tach200 <dev.tom404@gmail.com>
2022-12-30 16:06:17 +0000

    re-add amazon aws stuff

--- main.go
+++ main.go
@@ -1,24 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"go-pizza-api/internal/deals"
+
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
-// type PostCodeEvent struct {
-// 	Postcode string `json:"postcode"`
-// }
-
-// func HandleRequest(ctx context.Context, postcode PostCodeEvent) ([]deals.AllDeals, error) {
-// 	return deals.GetDeals(postcode.Postcode), nil
-// }
-
-// func main() {
-// 	lambda.Start(HandleRequest)
-// }
-
-// Easier for quick local testing
-func main() {
-	deals := deals.GetDeals("me46ea")
-	fmt.Printf("%+v", deals)
+type PostCodeEvent struct {
+	Postcode string `json:"postcode"`
 }
+
+func HandleRequest(ctx context.Context, postcode PostCodeEvent) ([]deals.AllDeals, error) {
+	return deals.GetDeals(postcode.Postcode), nil
+}
+
+func main() {
+	lambda.Start(HandleRequest)
+}
+
+// // Easier for quick local testing
+// func main() {
+// 	deals := deals.GetDeals("me46ea")
+// 	fmt.Printf("%+v", deals)
+// }
